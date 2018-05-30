b7fe90833b19f8485d5633480194a088807b2a07
idestion <m.sneekes@students.uu.nl>
2018-05-30 14:12:40 +0200

    Added twofactormatching, being a potential final algoritm matching 2 teams and a project

---
+++ Matching1/TwoFactorMatching.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matching1
+{
+    public class TwoFactorMatching
+    {
+        public TwoFactorMatching(List<Team> teams, Project project)
+        {
+            
+        }        
+    }
+}
