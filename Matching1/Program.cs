e353c534b2f82ca1f6488c21e1256e86979d65c9
iDestion <m.sneekes@students.uu.nl>
2018-06-20 18:29:15 +0200

    commit

--- Matching1/Program.cs
+++ Matching1/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
 
 namespace Matching1
 {
@@ -45,6 +46,23 @@ namespace Matching1
             TwoFactorMatcher matcher = new TwoFactorMatcher(teams, project);
             Match match = matcher.DoMatching();
             match.ToString();
+            
+            
+            
+            
+            
+            
+            WebServer ws = new WebServer(SendResponse, "http://localhost:8080/test/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
         }
+
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", DateTime.Now);    
+        }
+        
     }
 }
